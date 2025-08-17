af7d936d572322bcc67e26051253bfa06813f9d3
Estainlin Encarnacion <estainlinv@gmail.com>
2025-08-17 00:59:01 -0400

    Ultimos archivos

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Getion_de_Pasientes_de_Consultorio_Medico;
+
+namespace Getion_de_Pasientes_de_Consultorio_Medico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConexionAccess conexion = new ConexionAccess();
+            try
+            {
+                conexion.AbrirConeccin();
+                MessageBox.Show("Conexion exitosa");
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Error al conectar: " + ex.Message);
+            }
+
+            finally
+            {
+                conexion.CerrarConexion();
+            }
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Getion_de_Pasientes_de_Consultorio_Medico.Formularios;
+
+namespace Getion_de_Pasientes_de_Consultorio_Medico
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmLogin());
+        }
+    }
+}
